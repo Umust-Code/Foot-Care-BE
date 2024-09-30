baacb2c6444d1a400832cbc8aae2291719bf1fbc
Ryu-ji-hyeon <dudvndwlgus@naver.com>
2024-09-30 19:07:16 +0900

    first commit

---
+++ src/main/java/com/footcare/footcare/FootcareApplication.java
@@ -0,0 +1,13 @@
+package com.footcare.footcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/footcare/footcare/FootcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footcare.footcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
