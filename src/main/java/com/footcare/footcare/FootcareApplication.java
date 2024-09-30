4877b113f5615434a8ba6f5dc7fd98594ee0be4b
Ryu-ji-hyeon <dudvndwlgus@naver.com>
2024-09-30 19:08:23 +0900

    test

--- src/main/java/com/footcare/footcare/FootcareApplication.java
+++ src/main/java/com/footcare/footcare/FootcareApplication.java
@@ -2,7 +2,7 @@ package com.footcare.footcare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//foot care
 @SpringBootApplication
 public class FootcareApplication {
 
