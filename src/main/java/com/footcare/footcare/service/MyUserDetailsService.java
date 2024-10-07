9ee8f8d55060c70370bc2cf02b97cba8e7f884b9
Ryu-ji-hyeon <dudvndwlgus@naver.com>
2024-10-07 21:26:21 +0900

    fix : error

--- src/main/java/com/footcare/footcare/service/MyUserDetailsService.java
+++ src/main/java/com/footcare/footcare/service/MyUserDetailsService.java
@@ -1,11 +1,11 @@
 package com.footcare.footcare.service;
 
+import com.footcare.footcare.Repository.Member.MemberRepository;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.User;
 import com.footcare.footcare.entity.Member.Member;
-import com.footcare.footcare.Repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
