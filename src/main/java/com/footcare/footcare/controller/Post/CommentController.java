8277feb3d388b49a98cb42799d65eb132ccc3167
Ryu-ji-hyeon <dudvndwlgus@naver.com>
2024-11-07 22:28:59 +0900

    fix : cors error

--- src/main/java/com/footcare/footcare/controller/Post/CommentController.java
+++ src/main/java/com/footcare/footcare/controller/Post/CommentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "https://foot-care-fe.vercel.app")
 @RequestMapping("/api/comments")
 public class CommentController {
 
