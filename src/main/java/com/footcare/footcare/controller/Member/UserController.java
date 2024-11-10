9fbf6a7e4add52dfe24a8c53e0c96ffd3e5896eb
Ryu-ji-hyeon <dudvndwlgus@naver.com>
2024-11-10 23:56:55 +0900

    add : 최초 간이 설문조사 실시 여부 구현

--- src/main/java/com/footcare/footcare/controller/Member/UserController.java
+++ src/main/java/com/footcare/footcare/controller/Member/UserController.java
@@ -2,6 +2,7 @@ package com.footcare.footcare.controller.Member;
 
 import com.footcare.footcare.Repository.Member.MemberRepository;
 import com.footcare.footcare.entity.Member.Member;
+import com.footcare.footcare.service.Member.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,23 +11,41 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "https://foot-care-fe.vercel.app")
 @RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private MemberService memberService;
 
-    @DeleteMapping("/{userId}")
-    public ResponseEntity<?> deleteUser(@PathVariable Long userId, @RequestHeader("Authorization") String token) {
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long memberId, @RequestHeader("Authorization") String token) {
         // 토큰에서 ID 확인 및 인증 (필요한 경우 추가)
 
         // 해당 ID의 회원이 존재하는지 확인
-        Optional<Member> memberOptional = memberRepository.findById(userId);
+        Optional<Member> memberOptional = memberRepository.findById(memberId);
         if (memberOptional.isPresent()) {
-            memberRepository.deleteById(userId); // 회원 삭제
+            memberRepository.deleteById(memberId); // 회원 삭제
             return ResponseEntity.ok("User deleted successfully!");
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error: User not found.");
         }
+    }
+
+    @GetMapping("/{memberId}/check-survey")
+    public ResponseEntity<String> checkSurveyStatus(@PathVariable Long memberId) {
+        if (!memberService.isSurveyCompleted(memberId)) {
+            return ResponseEntity.ok("Redirect to survey page");
+        }
+        return ResponseEntity.ok("Survey already completed");
+    }
+
+
+    @PostMapping("/{memberId}/complete-survey")
+    public ResponseEntity<String> completeSurvey(@PathVariable Long memberId) {
+        memberService.completeSurvey(memberId);
+        return ResponseEntity.ok("Survey marked as completed");
     }
 }
