57f0f740a56f31c3b6ebb2a793ddb5dd81cb4b77
jinbobs <dkssusd90@naver.com>
2024-11-27 22:40:34 +0900

    add: 회원탈퇴

--- src/main/java/com/footcare/footcare/service/DiseaseSurvey/DiseaseSurveyService.java
+++ src/main/java/com/footcare/footcare/service/DiseaseSurvey/DiseaseSurveyService.java
@@ -1,11 +1,13 @@
 package com.footcare.footcare.service.DiseaseSurvey;
 
 import com.footcare.footcare.Repository.DiseaseSurvey.DiseaseSurveyRepository;
+import com.footcare.footcare.Repository.Member.MemberRepository;
 import com.footcare.footcare.Repository.Post.CategoryRepository;
 import com.footcare.footcare.dto.DiseaseSurveyDto.CategoryScoreDto;
 import com.footcare.footcare.dto.DiseaseSurveyDto.DiseaseSurveyDto;
 import com.footcare.footcare.dto.DiseaseSurveyDto.TotalScoreRequestDto;
 import com.footcare.footcare.entity.DiseaseSurvey.DiseaseSurvey;
+import com.footcare.footcare.entity.Member.Member;
 import com.footcare.footcare.entity.Post.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,9 +22,15 @@ public class DiseaseSurveyService {
     private DiseaseSurveyRepository diseaseSurveyRepository;
 
     @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
     private CategoryRepository categoryRepository;
 
     public void saveScores(TotalScoreRequestDto requestDto) {
+
+         Member member = memberRepository.findById(requestDto.getMemberId())
+                 .orElseThrow(() -> new IllegalArgumentException("Member not found"+requestDto.getMemberId()));
         for (CategoryScoreDto scoreDto : requestDto.getScores()) {
             // 각 카테고리 확인
             Category category = categoryRepository.findById(scoreDto.getCategoryId())
@@ -30,7 +38,7 @@ public class DiseaseSurveyService {
 
             DiseaseSurvey diseaseSurvey = new DiseaseSurvey();
             diseaseSurvey.setCategory(category);
-            diseaseSurvey.setMemberId(requestDto.getMemberId());
+            diseaseSurvey.setMember(member);
             diseaseSurvey.setDiseaseScore(String.valueOf(scoreDto.getScore()));
             diseaseSurvey.setDiseaseClassify(classifyScore(scoreDto.getScore()));
             diseaseSurvey.setDiseaseDate(new Date());
@@ -54,14 +62,14 @@ public class DiseaseSurveyService {
     }
 
     public List<DiseaseSurveyDto> getSurveysByMemberId(Long memberId) {
-        List<DiseaseSurvey> surveys = diseaseSurveyRepository.findByMemberId(memberId);
+        List<DiseaseSurvey> surveys = diseaseSurveyRepository.findByMember_MemberId(memberId);
         return surveys.stream()
                 .map(this::convertToDto)
                 .collect(Collectors.toList());
     }
 
     public List<DiseaseSurveyDto> getSurveysByMemberIdAndDate(Long memberId, Date date) {
-        List<DiseaseSurvey> surveys = diseaseSurveyRepository.findByMemberIdAndDiseaseDate(memberId, date);
+        List<DiseaseSurvey> surveys = diseaseSurveyRepository.findByMember_MemberIdAndDiseaseDate(memberId, date);
         return surveys.stream()
                 .map(this::convertToDto)
                 .collect(Collectors.toList());
@@ -73,7 +81,7 @@ public class DiseaseSurveyService {
 
     public List<Map<String, Object>> getAllSurveysGroupedByDate(Long memberId) {
         // 특정 사용자의 모든 설문조사 데이터 가져오기
-        List<DiseaseSurvey> surveys = diseaseSurveyRepository.findByMemberId(memberId);
+        List<DiseaseSurvey> surveys = diseaseSurveyRepository.findByMember_MemberId(memberId);
 
         // 날짜별로 그룹화
         Map<String, List<DiseaseSurvey>> groupedByDate = surveys.stream()
@@ -96,10 +104,12 @@ public class DiseaseSurveyService {
     }
 
     private DiseaseSurveyDto convertToDto(DiseaseSurvey survey) {
+
+
         DiseaseSurveyDto dto = new DiseaseSurveyDto();
         dto.setDiseaseSurveyId(survey.getDiseaseSurveyId());
         dto.setCategoryId(survey.getCategory().getCategoryId());
-        dto.setMemberId(survey.getMemberId());
+        dto.setMemberId(survey.getMember().getMemberId()); // Member 객체에서 ID 추출
         dto.setDiseaseScore(survey.getDiseaseScore());
         dto.setDiseaseClassify(survey.getDiseaseClassify());
         dto.setDiseaseDate(survey.getDiseaseDate());
